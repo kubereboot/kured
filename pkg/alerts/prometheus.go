8f98a999fee854c2336b7509de969183c7df77e1
simp <simp@saxobank.com>
2021-06-25 22:31:16 +0200

    revert the option to query by label

--- pkg/alerts/prometheus.go
+++ pkg/alerts/prometheus.go
@@ -7,12 +7,9 @@ import (
 	"sort"
 	"time"
 
-	log "github.com/sirupsen/logrus"
-
 	papi "github.com/prometheus/client_golang/api"
 	v1 "github.com/prometheus/client_golang/api/prometheus/v1"
 	"github.com/prometheus/common/model"
-	"k8s.io/apimachinery/pkg/api/errors"
 )
 
 // PromClient is a wrapper around the Prometheus Client interface and implements the api
@@ -29,7 +26,7 @@ func (p PromClient) New(conf papi.Config) (*PromClient, error) {
 
 	promClient, err := papi.NewClient(conf)
 	if err != nil {
-		return nil, errors.NewServiceUnavailable(err.Error())
+		return nil, err
 	}
 	client := PromClient{papi: promClient, api: v1.NewAPI(promClient)}
 	return &client, nil
@@ -40,8 +37,7 @@ func (p PromClient) New(conf papi.Config) (*PromClient, error) {
 // filter by regexp means when the regex finds the alert-name; the alert is exluded from the
 // block-list and will NOT block rebooting. query by includeLabel means,
 // if the query finds an alert, it will include it to the block-list and it WILL block rebooting.
-func (p *PromClient) ActiveAlerts(filter *regexp.Regexp, inclLabel map[string]string) ([]string, error) {
-	// var key, val string
+func (p *PromClient) ActiveAlerts(filter *regexp.Regexp) ([]string, error) {
 
 	// get all alerts from prometheus
 	value, _, err := p.api.Query(context.Background(), "ALERTS", time.Now())
@@ -49,26 +45,13 @@ func (p *PromClient) ActiveAlerts(filter *regexp.Regexp, inclLabel map[string]st
 		return nil, err
 	}
 
-	// TODO: create seperate functions to make it more readable / understand.
 	if value.Type() == model.ValVector {
 		if vector, ok := value.(model.Vector); ok {
 			activeAlertSet := make(map[string]bool)
 			for _, sample := range vector {
 				if alertName, isAlert := sample.Metric[model.AlertNameLabel]; isAlert && sample.Value != 0 {
 					if filter == nil || !filter.MatchString(string(alertName)) {
-						log.Info("Added to the BlockingList, by regexFilter: ", alertName)
 						activeAlertSet[string(alertName)] = true
-					}
-					for k, v := range inclLabel {
-						if sample.Metric[model.LabelName(k)] == model.LabelValue(v) {
-							// because filtering by labels doesn't check for alertname, we want to make sure
-							// there's no deadlock, that is, Kured is alerting itself on the block-list.
-							if alertName == "RebootRequired" {
-								continue
-							}
-							activeAlertSet[string(alertName)] = true
-							log.Info("Added to the BlockingList, by includeLabel: ", alertName)
-						}
 					}
 				}
 			}
