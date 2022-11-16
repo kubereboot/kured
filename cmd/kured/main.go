79b54b4b86b44c024512bab242ef33f15dbde9b9
Aditya Sundaramurthy <aditya.sundaramurthy@maersk.com>
2022-11-16 10:59:28 +0100

    Fix conflicts
    Signed-off-by: Aditya Sundaramurthy <aditya.sundaramurthy@maersk.com>

--- cmd/kured/main.go
+++ cmd/kured/main.go
@@ -197,11 +197,7 @@ func NewRootCommand() *cobra.Command {
 		"labels to add to nodes before cordoning")
 	rootCmd.PersistentFlags().StringSliceVar(&postRebootNodeLabels, "post-reboot-node-labels", nil,
 		"labels to add to nodes after uncordoning")
-<<<<<<< HEAD
 	rootCmd.PersistentFlags().Uint16Var(&metricsPort, "metrics-port", 8080, "port where the /metrics endpoint is served. Defaults to 8080")
-=======
-	rootCmd.PersistentFlags().Uint16Var(&metricsPort, "metrics-port", 8080, "port where the /metrics endpoint is served")
->>>>>>> b24f0cf (Make metrics port configurable)
 	return rootCmd
 }
 
