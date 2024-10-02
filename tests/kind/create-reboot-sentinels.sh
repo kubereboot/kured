a02ae67559b438ca03617a52718e781185db1661
Jean-Philippe Evrard <open-source@a.spamming.party>
2024-10-02 23:26:33 +0200

    Accelerate CI jobs
    Without this, some CI jobs are flaky or slow due to the following
    issues:
    - Triggering a reboot cause an unrecoverable boot loop.
      This fixes it by restarting the containers that are incorrectly
      exited.
    - API server is down while operations happen.
      This fixes it by ensuring at least one API server is up. In this
      case, we don't add a reboot marker on the unique api server.
    - The amount of nodes in a test environment is larger than
      necessary.
      This fixes it by ensuring two nodes are required to reboot.
      This is enough for concurrency, and for the e2e testing.
    - The wait time between operations is high, and can cause
      a heartbeat to be missed in the check script.
      This fixes it by checking more often, at the expense of
      more logging. This is compensated by increasing the amount
      of tries.
    
    Signed-off-by: Jean-Philippe Evrard <open-source@a.spamming.party>

--- tests/kind/create-reboot-sentinels.sh
+++ tests/kind/create-reboot-sentinels.sh
@@ -4,9 +4,10 @@
 KUBECTL_CMD="${KUBECTL_CMD:-kubectl}"
 SENTINEL_FILE="${SENTINEL_FILE:-/var/run/reboot-required}"
 
-echo "Creating reboot sentinel on all nodes"
+echo "Creating reboot sentinel on worker nodes"
 
-for nodename in $("$KUBECTL_CMD" get nodes -o name); do
+# To speed up the system, let's not kill the control plane.
+for nodename in $("$KUBECTL_CMD" get nodes -o name | grep -v control-plane); do
     docker exec "${nodename/node\//}" hostname
     docker exec "${nodename/node\//}" touch "${SENTINEL_FILE}"
 done
