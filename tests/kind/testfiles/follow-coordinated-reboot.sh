a53429cf960f87f0bd12b200aa2d73b3b627a742
Jean-Philippe Evrard <open-source@a.spamming.party>
2024-10-06 16:02:22 +0200

    Create e2e from go tests interface
    Without this, e2e tests need tons of manual work to
    test locally, and the results are not easily exposed.
    
    People are less likely to use the e2e tests if they
    are tough to use outside the CI.
    
    This commit makes it easier to run tests locally,
    and ensures the CI is closer to the Makefile.
    
    At the same time, this removes debt in the github
    worfklows: By switching to newer versions of kind,
    we can remove the very old workaround for the
    failed to attach pid 1.
    
    Signed-off-by: Jean-Philippe Evrard <open-source@a.spamming.party>

--- tests/kind/testfiles/follow-coordinated-reboot.sh
+++ tests/kind/testfiles/follow-coordinated-reboot.sh
@@ -1,11 +1,14 @@
 #!/usr/bin/env bash
 
-NODECOUNT=${NODECOUNT:-2}
-KUBECTL_CMD="${KUBECTL_CMD:-kubectl}"
+REBOOTCOUNT=${REBOOTCOUNT:-2} # By default we only create two sentinels in create-reboot-sentinels.
 DEBUG="${DEBUG:-false}"
 CONTAINER_NAME_FORMAT=${CONTAINER_NAME_FORMAT:-"chart-testing-*"}
 
+kubectl_flags=( )
+[[ "$1" != "" ]] && kubectl_flags=("${kubectl_flags[@]}" --context "$1")
+
 tmp_dir=$(mktemp -d -t kured-XXXX)
+
 function gather_logs_and_cleanup {
     if [[ -f "$tmp_dir"/node_output ]]; then
         rm "$tmp_dir"/node_output
@@ -18,15 +21,15 @@ function gather_logs_and_cleanup {
         # This is useful to see if containers have crashed.
         echo "docker ps -a:"
         docker ps -a
-	echo "docker journal logs"
-	journalctl -u docker --no-pager
+	      echo "docker journal logs"
+	      journalctl -u docker --no-pager
 
         # This is useful to see if the nodes have _properly_ rebooted.
         # It should show the reboot/two container starts per node.
-        for name in $(docker ps -a -f "name=${CONTAINER_NAME_FORMAT}" -q); do
+        for id in $(docker ps -a -q); do
             echo "############################################################"
-            echo "docker logs for container $name:"
-            docker logs "$name"
+            echo "docker logs for container $id:"
+            docker logs "$id"
         done
 
     fi
@@ -42,23 +45,18 @@ attempt_num=1
 # Get docker info of each of those kind containers. If one has crashed, restart it.
 
 set +o errexit
-echo "There are $NODECOUNT nodes in the cluster"
-until [ ${#was_unschedulable[@]} == "$NODECOUNT" ] && [ ${#has_recovered[@]} == "$NODECOUNT" ]
+echo "There are $REBOOTCOUNT nodes total needing reboot in the cluster"
+until [ ${#was_unschedulable[@]} == "$REBOOTCOUNT" ] && [ ${#has_recovered[@]} == "$REBOOTCOUNT" ]
 do
     echo "${#was_unschedulable[@]} nodes were removed from pool once:" "${!was_unschedulable[@]}"
     echo "${#has_recovered[@]} nodes removed from the pool are now back:" "${!has_recovered[@]}"
 
-    #"$KUBECTL_CMD" logs -n kube-system -l name=kured --ignore-errors > "$tmp_dir"/node_output
-    #if [[ "$DEBUG" == "true" ]]; then
-    #    echo "Kured pod logs:"
-    #    cat "$tmp_dir"/node_output
-    #fi
 
-    "$KUBECTL_CMD" get nodes -o custom-columns=NAME:.metadata.name,SCHEDULABLE:.spec.unschedulable --no-headers | grep -v control-plane > "$tmp_dir"/node_output
+    ${KUBECTL_CMD:-kubectl} "${kubectl_flags[@]}" get nodes -o custom-columns=NAME:.metadata.name,SCHEDULABLE:.spec.unschedulable --no-headers | grep -v control-plane > "$tmp_dir"/node_output
     if [[ "$DEBUG" == "true" ]]; then
         # This is useful to see if a node gets stuck after drain, and doesn't
         # come back up.
-        echo "Result of command $KUBECTL_CMD get nodes ... showing unschedulable nodes:"
+        echo "Result of command kubectl unschedulable nodes:"
         cat "$tmp_dir"/node_output
     fi
 
@@ -81,7 +79,7 @@ do
 
     done < "$tmp_dir"/node_output
 
-    if [[ "${#has_recovered[@]}" == "$NODECOUNT" ]]; then
+    if [[ "${#has_recovered[@]}" == "$REBOOTCOUNT" ]]; then
         echo "All nodes recovered."
         break
     else
