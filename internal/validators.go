54db32e137b52b44f1de71e0156ab4e25f1cf0d4
Jean-Philippe Evrard <open-source@a.spamming.party>
2024-11-08 20:59:20 +0100

    Remove unnecessary functions
    delaytick is not used anymore.
    
    Signed-off-by: Jean-Philippe Evrard <open-source@a.spamming.party>
    
    # Conflicts:
    #	internal/delaytick/delaytick.go
    #	internal/validators.go

--- internal/validators.go
+++
@@ -1,6 +0,0 @@
-// Package internal provides convenient tools which shouldn't be in the cmd/main
-// It will eventually provide internal validation and chaining logic to select
-// appropriate reboot and sentinel check methods based on configuration.
-// It validates user input and instantiates the correct checker and rebooter implementations
-// for use elsewhere in kured.
-package internal
